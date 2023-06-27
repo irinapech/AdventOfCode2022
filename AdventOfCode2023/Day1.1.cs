48a9324e4ef40db81313fe88543ae3672bcb8780
irina.pech <iren.pech13@gmail.com>
2023-06-27 17:49:48 -0500

    Add project files.

---
+++ AdventOfCode2023/Day1.1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public static class Day1_1
+    {
+        public static void Execute()
+        {
+            String line;
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\DSU\\source\\repos\\AdventOfCode2023\\AdventOfCode2023\\Day1.txt");
+                line = sr.ReadLine();
+
+                double maxCalories = 0;
+                double sumOfCalories = 0;
+
+                while (line != null)
+                {
+                    while (!string.IsNullOrEmpty(line))
+                    {
+                        sumOfCalories += Convert.ToDouble(line);
+                        Console.WriteLine(line);
+                        line = sr.ReadLine();
+                    }
+                    if (sumOfCalories > maxCalories)
+                    {
+                        maxCalories = sumOfCalories;
+                        Console.WriteLine("New max calories: " + maxCalories);
+                    }
+                    sumOfCalories = 0;
+                    line = sr.ReadLine();
+                }
+                Console.WriteLine("Max amount of calories: " + maxCalories);
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace AdventOfCode2022.Solutions
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Day1_1.Execute();
+        }
+    }
+}
