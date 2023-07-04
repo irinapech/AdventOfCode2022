f50fe8d075f2804409a61dd9a2d5824cb84296c4
irinapech <120232888+irinapech@users.noreply.github.com>
2023-07-04 15:31:45 -0500

    Working on the solution for Day 7 part 1. Still not getting correct answer

--- AdventOfCode2022/Day7_1.cs
+++ AdventOfCode2022/Day7_1.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace AdventOfCode2022
 {
@@ -10,7 +7,7 @@ namespace AdventOfCode2022
     {
         public static void Execute()
         {
-            String line;
+            string line;
             try
             {
                 StreamReader sr = new StreamReader("C:\\Users\\DSU\\OneDrive - Dakota State University\\Desktop\\repos\\AdventOfCode2022\\AdventOfCode2022\\Day7.txt");
@@ -22,6 +19,9 @@ namespace AdventOfCode2022
 
                 string currentDirectory = "/";
                 string directoryName;
+
+                int totalSizeOfAtMost100000 = 0;
+                int maxSize = 100000;
 
                 while (line != null)
                 {
@@ -46,28 +46,40 @@ namespace AdventOfCode2022
                                 int size = Convert.ToInt32(lineSplit[0]);
                                 fileSizes[currentDirectory] += size;
                             }
-                            if (line.Contains("dir"))
-                            {
-                                directoryName = line.Substring(line.IndexOf(' '));
-                                directoriesNames[currentDirectory].Add(directoryName);
-                                directoriesNames[directoryName] = new List<string>();
-                            }
+                            //if (line.Contains("dir"))
+                            //{
+                            //    directoryName = line.Substring(line.IndexOf(' '));
+                            //    directoriesNames[currentDirectory].Add(directoryName);
+                            //}
                             line = sr.ReadLine();
                         }
                     }
                     if (line != null && line.Contains("cd") && !line.Contains(".."))
                     {
-                        currentDirectory = line.Substring(line.IndexOf('d') + 1);
-                        directoryName = currentDirectory;
-                        fileSizes[directoryName] = 0;
+                        directoryName = line.Substring(line.IndexOf('d') + 1);
+                        directoriesNames[currentDirectory].Add(directoryName);
+                        currentDirectory = directoryName;
+                        directoriesNames[currentDirectory] = new List<string>();
+                        fileSizes[currentDirectory] = 0;
                     }
 
                     if (line != null && line == "$ cd ..")
                     {
+                        foreach(string directory in directoriesNames[currentDirectory])
+                        {
+                            fileSizes[currentDirectory] += fileSizes[directory];
+                        }
+                        
+                        if (fileSizes[currentDirectory] <= maxSize)
+                        {
+                            totalSizeOfAtMost100000 += fileSizes[currentDirectory];
+                        }
+
                         foreach (KeyValuePair <string, List<string>> pair in directoriesNames)
                         {
                             if (pair.Value.Contains(currentDirectory))
                             {
+                                fileSizes[pair.Key] += fileSizes[currentDirectory];
                                 currentDirectory = pair.Key;
                                 break;
                             }
@@ -75,54 +87,23 @@ namespace AdventOfCode2022
                     }
                     line = sr.ReadLine();
                 }
-                Console.WriteLine();
-                foreach (KeyValuePair<string, int> pair in fileSizes)
-                {
-                    Console.WriteLine("\nKey = {0}, Value = {1}", pair.Key, pair.Value);
-                }
 
-                Console.WriteLine();
-                foreach (KeyValuePair<string, List<string>> pair in directoriesNames)
-                {
-                    Console.WriteLine("\nKey = {0}", pair.Key);
-                    foreach (string directory in  pair.Value)
-                    {
-                        Console.WriteLine(directory);
-                    }
-                }
+                //Console.WriteLine();
+                //foreach (KeyValuePair<string, List<string>> pair in directoriesNames)
+                //{
+                //    Console.WriteLine("\nKey = {0}", pair.Key);
+                //    foreach (string directory in  pair.Value)
+                //    {
+                //        Console.WriteLine(directory);
+                //    }
+                //}
 
-                foreach (KeyValuePair<string, List<string>> pair in directoriesNames)
-                {
-                    if (pair.Key == "/")
-                    {
-                        continue;
-                    }
-                    foreach (string directory in pair.Value)
-                    {
-                        fileSizes[pair.Key] += fileSizes[directory];
-                    }
-                }
-                foreach (string directory in directoriesNames["/"])
-                {
-                    fileSizes["/"] += fileSizes[directory];
-                }
+                //Console.WriteLine();
+                //foreach (KeyValuePair<string, int> pair in fileSizes)
+                //{
+                //    Console.WriteLine("\nKey = {0}, Value = {1}", pair.Key, pair.Value);
+                //}
 
-                Console.WriteLine();
-                foreach (KeyValuePair<string, int> pair in fileSizes)
-                {
-                    Console.WriteLine("\nKey = {0}, Value = {1}", pair.Key, pair.Value);
-                }
-
-                int totalSizeOfAtMost100000 = 0;
-                int maxSize = 100000;
-                Console.WriteLine();
-                foreach (KeyValuePair<string, int> pair in fileSizes)
-                {
-                    if (fileSizes[pair.Key] <= maxSize)
-                    {
-                        totalSizeOfAtMost100000 += fileSizes[pair.Key];
-                    }
-                }
                 Console.WriteLine("The sum of total sizes of directories < 100000: " + totalSizeOfAtMost100000);
 
                 sr.Close();
