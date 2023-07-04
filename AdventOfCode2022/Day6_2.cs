261fcffec2c9f763999e617db6bf789b2b1ff687
irina.pech <120232888+irinapech@users.noreply.github.com>
2023-07-03 21:18:41 -0500

    Added solutions for Day 6 part 1 and part 2

---
+++ AdventOfCode2022/Day6_2.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventOfCode2022
+{
+    public static class Day6_2
+    {
+        public static void Execute()
+        {
+            static bool OnlyOnceCheck(string input)
+            {
+                return input.GroupBy(x => x).Any(g => g.Count() > 1);
+            }
+
+            String line;
+            int numberOfCharsInMarker = 14;
+
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\DSU\\OneDrive - Dakota State University\\Desktop\\repos\\AdventOfCode2022\\AdventOfCode2022\\Day6.txt");
+
+                line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+
+                    for (int i = 0; i < line.Length - numberOfCharsInMarker; i++)
+                    {
+                        string marker = line.Substring(i, numberOfCharsInMarker);
+                        if (!OnlyOnceCheck(marker))
+                        {
+                            Console.WriteLine(i + numberOfCharsInMarker);
+                            break;
+                        }
+                    }
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,13 +1,10 @@
-﻿using System.IO;
-using System.Security.Cryptography;
-
-namespace AdventOfCode2022.Solutions
+﻿namespace AdventOfCode2022.Solutions
 {
     public static class Program
     {
         public static void Main()
         {
-            Day5_2.Execute();
+            Day6_2.Execute();
         }
     }
 }
